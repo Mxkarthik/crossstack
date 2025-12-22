0d662b3ad6ef441a60b69119191e94ef6eb07824
Karthik Medapalli <maxwhitie1234@gmail.com>
2025-12-22 18:19:12 +0530

    otp generated using math.random()

--- auth-service/backend/controllers/user.js
+++ auth-service/backend/controllers/user.js
@@ -7,6 +7,9 @@ import bcrypt from "bcrypt";
 import crypto from "crypto";
 import sendMail from "../config/sendMail.js";
 import { getVerifyEmailHtml } from "../config/html.js";
+import { loginSchema } from "../config/zod.js";
+import { getOtpHtml } from "../config/html.js";
+
 
 export const registerUser = TryCatch(async(req,res)=>{
     const sanitizedBody = sanitize(req.body);
@@ -124,4 +127,80 @@ export const verifyUser = TryCatch(async(req,res)=>{
     });
 });
 
+export const loginUser = TryCatch(async(req,res)=>{
+     const sanitizedBody = sanitize(req.body);
+
+    const validation = loginSchema.safeParse(sanitizedBody);
+
+    if(!validation.success) {
+        const zodError = validation.error;
+
+        let firstErrorMessage = "Validation Failed"
+        let allErrors = [];
+
+        if(zodError?.issues && Array.isArray(zodError.issues)) {
+            allErrors = zodError.issues.map((issue)=>({
+                field: issue.path ? issue.path.join("."):"unknown",
+                message: issue.message || "Validation Error",
+                code: issue.code,
+            }));
+
+            firstErrorMessage = allErrors[0].message || "Validation Error";
+        }
+        return res.status(400).json({
+            message: firstErrorMessage,
+            error: allErrors,
+        });
+    }
+
+    const {name ,email ,password} = validation.data;
+
+    const rateLimitKey = `login-rate-limit:${req.ip}:${email}`;
+
+        if(await redisClient.get(rateLimitKey) ){
+        return res.status(429).json({
+              message:"Too Many Requests, try again later",
+            });
+        }
+
+    const user = await User.findOne({email})
+    
+    if(!user){
+        return res.status(400).json({
+            message:"Invalid credentials",
+
+        });
+    }
+
+    const comparePassword = await bcrypt.compare(password,user.password);
+
+    if(!comparePassword) {
+         return res.status(400).json({
+            message:"Invalid credentials",
+
+        });
+    }
+
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const otpKey = `otp:${email}`;
+
+    await redisClient.set(otpKey, JSON.stringify(otp), {
+        EX: 300,
+    });
+
+    const subject = "Otp for verification";
+
+    const html = getOtpHtml({email,otp});
+
+    await sendMail({email,subject,html});
+
+    await redisClient.set(rateLimitKey,"true" ,{
+        EX:60
+    });
+
+    res.json({
+        message: "If your email is valid, an otp has been sent. it will be valid for 5 minutes",
+    });
+});
 
