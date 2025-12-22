f1109b5a2bb49970f410dec37394bb92d467e439
Karthik Medapalli <maxwhitie1234@gmail.com>
2025-12-22 13:36:42 +0530

    imported monogo santisizer for preventing sql injections

--- auth-service/backend/controllers/user.js
+++ auth-service/backend/controllers/user.js
@@ -1,7 +1,8 @@
 import TryCatch from "../middlewares/TryCatch.js";
+import sanitize from "mongo-sanitize";
 
 export const registerUser = TryCatch(async(req,res)=>{
-    const {name,email,password} = req.body;
+    const {name,email,password} = sanitize(req.body);
 
     res.json({
         name,
