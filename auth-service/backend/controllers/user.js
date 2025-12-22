61783f5f7b28b7ce10650ad9aee5b010c3974cc5
Karthik Medapalli <maxwhitie1234@gmail.com>
2025-12-22 14:00:40 +0530

    initialised validation using zod

--- auth-service/backend/controllers/user.js
+++ auth-service/backend/controllers/user.js
@@ -1,12 +1,23 @@
+import { registerSchema } from "../config/zod.js"
 import TryCatch from "../middlewares/TryCatch.js";
 import sanitize from "mongo-sanitize";
 
 export const registerUser = TryCatch(async(req,res)=>{
-    const {name,email,password} = sanitize(req.body);
+    const sanitizedBody = sanitize(req.body);
+
+    const validation = registerSchema.safeParse(sanitizedBody);
+
+    if(!validation.success) {
+        return res.status(400).json({
+            message: "validation error",
+        });
+    }
+
+    const {name ,email ,password} = validation.data;
 
     res.json({
         name,
         email,
         password,
-    })
+    });
 });
