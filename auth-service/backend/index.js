34ac40346507b7d28f8f553964908f64b1314392
Karthik Medapalli <maxwhitie1234@gmail.com>
2025-12-22 13:01:33 +0530

    data schema added

--- auth-service/backend/index.js
+++ auth-service/backend/index.js
@@ -9,6 +9,12 @@ await connectDB();
 
 const app = express();
 
+//importing routes 
+import userRoutes from './routes/user.js';
+
+// using routes 
+app.use("/api/v1", userRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , ()=>{
